3b71b17c14b2373072bfb4ed73ff62f918116434
okyska <okyska@yahoo.com>
2018-02-21 15:26:52 -0700

    save places

--- app/src/main/java/com/example/max/memorableplaces/MainActivity.java
+++ app/src/main/java/com/example/max/memorableplaces/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.max.memorableplaces;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,12 +28,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.max.memorableplaces", Context.MODE_PRIVATE);
+
+        ArrayList<String> latitudes = new ArrayList<>();
+        ArrayList<String> longitudes = new ArrayList<>();
+        places.clear();
+        latitudes.clear();
+        longitudes.clear();
+        locations.clear();
+
+        try {
+
+            places = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("places", ObjectSerializer.serialize(new ArrayList<String>())));
+            latitudes = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("lats", ObjectSerializer.serialize(new ArrayList<String>())));
+            longitudes = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("lons", ObjectSerializer.serialize(new ArrayList<String>())));
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        if(places.size()>0 && latitudes.size() >0 && longitudes.size()>0){
+            if(places.size() == latitudes.size() && places.size() == longitudes.size()){
+                for (int i=0; i < latitudes.size(); i++) {
+                    locations.add(new LatLng(Double.parseDouble(latitudes.get(i)), Double.parseDouble(longitudes.get(i))));
+
+                }
+            }
+
+        } else {
+            places.add("Add a new place...");
+            locations.add(new LatLng(0, 0));
+        }
+
+
+
         ListView listView = findViewById(R.id.listView);
 
 
 
-        places.add("Add a new place...");
-        locations.add(new LatLng(0, 0));
+
 
         places.add("Add a new place...");
 
